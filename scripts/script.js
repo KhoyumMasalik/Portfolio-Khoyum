fb887d9f1feb7cd3f048a2daeb038d5f6173cb23
KhoyumMasalik <abdulkuyummasalik@smkwikrama.sch.id>
2023-11-16 22:36:01 +0700

    six

--- scripts/script.js
+++ scripts/script.js
@@ -47,8 +47,8 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portofolio-box, .contact form', {origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-img, .services-container, .education-container, .portofolio-box, .contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img',  {origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 
 // typed
